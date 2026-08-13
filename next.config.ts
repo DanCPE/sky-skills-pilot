f7abc17fb273eb091c7d65303ab973b6700d12b6
DanCPE <dan615415@gmail.com>
2026-08-13 16:46:38 +0700

    feat: add experimental configuration for proxy client max body size

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    proxyClientMaxBodySize: "120mb",
+  },
   outputFileTracingIncludes: {
     "/api/approximation/questions": ["./src/lib/approximation-engine/**/*.py"],
   },
